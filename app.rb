06db6f661e3456d42d68f3539d8ee06bec5f9cc5
JohnZBurnett <tuninator@gmail.com>
2018-06-05 10:41:38 -0400

    update solution

--- app.rb
+++ app.rb
@@ -12,6 +12,9 @@ class App < Sinatra::Base
 
   post '/new' do
     @pup = Puppy.new(params[:name], params[:breed], params[:age])
+    @name = @pup.name
+    @breed = @pup.breed
+    @age = @pup.age
     erb :display_puppy
   end
 end
